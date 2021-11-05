d302b89f9042c99d71200bb5220a6429aaed54f1
Nico Pinguel <nico.mp215@gmail.com>
2021-11-04 23:14:41 -0500

    finance insert

--- nico_car_dealership_insert.sql
+++ nico_car_dealership_insert.sql
@@ -1,3 +1,56 @@
+-- Insert for customer table
+INSERT INTO customer(
+	customer_id,
+	first_name,
+	last_name
+)VALUES(
+	1,
+	'Ramon',
+	'Goose'
+);
+SELECT *
+FROM customer;
+
+-- Insert for sales_people table
+INSERT INTO sales_people(
+	sales_person_id,
+	first_name,
+	last_name
+)VALUES(
+	1,
+	'Tony',
+	'Jones'
+);
+-- SELECT *
+-- FROM sales_people;
+
+-- Insert for mechanic table
+INSERT INTO mechanic(
+	mechanic_id,
+	first_name,
+	last_name 
+)VALUES(
+	1,
+	'Sam',
+	'DeGeneres'
+);
+SELECT *
+FROM mechanic;
+
+-- Insert for car table
+INSERT INTO finance(
+	finance_id,
+	finance_type,
+	institution_name
+)VALUES(
+	1,
+	'Loan',
+	'Chuck E Bank'
+);
+SELECT *
+FROM finance;
+
+
 -- Insert for car table
 INSERT INTO car(
 	vin,
