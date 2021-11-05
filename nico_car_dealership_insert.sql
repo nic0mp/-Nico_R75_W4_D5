865de735c87f6fd2e8180d8ee42242fa280b2ad4
Nico Pinguel <nico.mp215@gmail.com>
2021-11-04 22:43:38 -0500

    car insert

---
+++ nico_car_dealership_insert.sql
@@ -0,0 +1,22 @@
+-- Insert for car table
+INSERT INTO car(
+	vin,
+	make,
+	model,
+	car_year,
+	miles,
+	last_updated,
+	car_condition,
+	customer_id
+) VALUES(
+	00000000001,
+	'Honda',
+	'Accord',
+	'2022',
+	'45',
+	'2021-10-01',
+	'New',
+	1
+);
+-- SELECT *
+-- FROM customer;
